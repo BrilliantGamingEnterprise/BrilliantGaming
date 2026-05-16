c839963c177aed77ad1950996dc8f772710a1a3a
BrilliantGaming <brilliantgaming.bg@gmail.com>
2026-05-17 01:42:52 +0800

    删除data非法文件名修复Netlify部署

--- data/#U56fd#U9645#U670d#U6e38#U620f__#U5c0f#U9e21#U5e01__#U5f39#U5f39#U5802.js
+++
@@ -1,9 +0,0 @@
-window.productList = [
-  { name: '18 小鸡币', rm: 64, sgd: 21, img: '1.png' },
-  { name: '36 小鸡币', rm: 121, sgd: 40, img: '2.png' },
-  { name: '55 小鸡币', rm: 185, sgd: 61, img: '3.png' },
-  { name: '75 小鸡币', rm: 252, sgd: 84, img: '4.png' },
-  { name: '100 小鸡币', rm: 306, sgd: 99, img: '5.png' },
-  { name: '200 小鸡币', rm: 574, sgd: 185, img: '6.png' },
-  { name: '400 小鸡币', rm: 1147, sgd: 370, img: '7.png' }
-];
--- data/#U56fd#U9645#U670d#U6e38#U620f__Pokemon Tcgp__Pokemon Tcg.js
+++
@@ -1,9 +0,0 @@
-window.productList = [
-  { name: '5<br>GOLD BAR', rm: 4, sgd: 1.3, img: '1.png' },
-  { name: '25 + 1<br>GOLD BAR', rm: 18, sgd: 6, img: '2.png' },
-  { name: '50 + 7<br>GOLD BAR', rm: 36, sgd: 12, img: '3.png' },
-  { name: '100 + 20<br>GOLD BAR', rm: 72, sgd: 24, img: '4.png' },
-  { name: '200 + 50<br>GOLD BAR', rm: 140, sgd: 46, img: '5.png' },
-  { name: '500 + 190<br>GOLD BAR', rm: 320, sgd: 106, img: '6.png' },
-  { name: '115 GOLD BAR <br>+ ACCESSORIES', rm: 83, sgd: 28, img: '礼包.png' },
-];
